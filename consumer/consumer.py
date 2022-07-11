df36c1393dd0f6da8e9586e325164884d17a4feb
wr-araujo <william.araujo@indt.org.br>
2022-07-10 21:37:23 -0400

    add rabbitmq Dockerfile

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,18 +1,30 @@
 #!/usr/bin/env python
 import pika
+import os
+from time import sleep
 import json
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq.com'))
+def pdf_process_function(msg):
+    print(" PDF processing")
+    print(" [x] Received " + str(msg))
+    sleep(5) # delay for 5 seconds
+    print(" PDF processing finished")
+    return
+
+# Access the CLOUD_AMQP_URL environment variable and parse it (fallback to localhost)
+url = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@127.0.0.2:5672/%2f')
+print(url)
+params = pika.URLParameters(url)
+params.socket_timeout = 10
+connection = pika.BlockingConnection(params)
 
 channel = connection.channel()
 
 channel.queue_declare(queue='rpc_queue')
 
-
 def message_to_send():
     message = dict(service='service-received', body="Sou o listening")
     return json.dumps(message)
-
 
 def on_request(ch, method, props, body):
     message_received = json.loads(body)
@@ -23,8 +35,7 @@ def on_request(ch, method, props, body):
 
     ch.basic_publish(exchange='',
                      routing_key=props.reply_to,
-                     properties=pika.BasicProperties(correlation_id = \
-                                                         props.correlation_id),
+                     properties=pika.BasicProperties(correlation_id = props.correlation_id),
                      body=str(response))
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
